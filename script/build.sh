132a585fb240c86bda7fe2d8f4e96d83bdcd27f2
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-08-19 14:51:59 -0700

    Build dist files in a prepublish step
    Don't check them in ya big dummy!

--- script/build.sh
+++ script/build.sh
@@ -3,6 +3,7 @@ set -ex
 
 # Browserify
 echo "Browserifying..."
+mkdir -p ./dist
 browserify -r $npm_package_main:justified-layout > ./dist/justified-layout.js
 uglifyjs ./dist/justified-layout.js -o ./dist/justified-layout.min.js
 
