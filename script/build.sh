5ef2ac8e2f78bf6655b1d00fad0f6f1dec11e5c8
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-08-19 15:56:36 -0700

    Move build script into npm scripts

--- script/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# Browserify
-echo "Browserifying..."
-mkdir -p ./dist
-browserify -r $npm_package_main:justified-layout > ./dist/justified-layout.js
-uglifyjs ./dist/justified-layout.js --comments -o ./dist/justified-layout.min.js
